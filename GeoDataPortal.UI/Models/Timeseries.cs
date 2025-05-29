5b424308f1799eeee61a96b1edad9c35d20fca52
Reno Satya Adrian <reno.satyaadrian@ai.astra.co.id>
2025-05-29 16:34:14 +0700

    feat(ui): add type and unit in ui

--- GeoDataPortal.UI/Models/Timeseries.cs
+++ GeoDataPortal.UI/Models/Timeseries.cs
@@ -6,6 +6,8 @@ namespace GeoDataPortal.UI.Models
         public Guid GeoDataId { get; set; }
         public DateTime Timestamp { get; set; }
         public double Value { get; set; }
+        public string Type { get; set; } = string.Empty;
+        public string Unit { get; set; } = string.Empty;
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
     }
 }
