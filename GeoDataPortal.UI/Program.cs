a1d49d8ebcae1c0e6b7dfb6f5e00edbd269a57c3
Reno Satya Adrian <reno.satyaadrian@ai.astra.co.id>
2025-05-29 11:30:51 +0700

    fix(ui): http client and uri

--- GeoDataPortal.UI/Program.cs
+++ GeoDataPortal.UI/Program.cs
@@ -1,20 +1,22 @@
+using GeoDataPortal.UI.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddScoped<UserService>();
+
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.AddHttpClient("GeoDataPortal.API", client =>
+builder.Services.AddHttpClient<UserService>(client =>
 {
-    client.BaseAddress = new Uri("https://localhost:7020/api/");
+    client.BaseAddress = new Uri("https://localhost:7020/");
 });
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
